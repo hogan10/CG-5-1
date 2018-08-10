7993000f02a7e410d47e133f4cfbf2ff40cc4cf4
Erin Hogan <hogan10@charter.net>
2018-08-09 22:51:34 -0500

    need to add comments

---
+++ CG 5-1/CG 5-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CG_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Your name: ");
+            PrintFirstLast(firstName);
+            PrintFirstLast(lastName);
+            Console.ReadLine();
+        }
+
+        private static void PrintFirstLast(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+            Console.Write(" ");
+         
+        }
+    }
+}
