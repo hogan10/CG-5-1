beac6fab1754436e2b66f090ad7a5b76b2aaad0d
Erin Hogan <hogan10@charter.net>
2018-08-10 14:21:07 -0500

    with comments

--- CG 5-1/CG 5-1/Program.cs
+++ CG 5-1/CG 5-1/Program.cs
@@ -6,18 +6,23 @@ namespace CG_5_1
     {
         static void Main(string[] args)
         {
+            //We first ask them to enter their first name
             Console.WriteLine("Please enter your first name: ");
             string firstName = Console.ReadLine();
-
+            //next we ask them to enter their last name
             Console.WriteLine("Please enter your last name: ");
             string lastName = Console.ReadLine();
 
+            //finally we want the name to be written back onto the screen. This can be done using a second method and calling the method below
             Console.Write("Your name: ");
             PrintFirstLast(firstName);
             PrintFirstLast(lastName);
             Console.ReadLine();
         }
-
+        /// <summary>
+        /// this method writes the full name out above
+        /// </summary>
+        /// <param name="message"></param>
         private static void PrintFirstLast(string message)
         {
             char[] messageArray = message.ToCharArray();
